665b77eae9fb2ae7580f953b935f0db51686963f
jesus <jesus@dm4>
2015-05-05 23:29:52 +0200

    dsfsf

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+var_dump((bool)0.0);
+var_dump((bool)0);
+var_dump((bool)"0");
+
+//echo rand(0,10); 
+//o cast sempre a esquerda obviamente
+echo((integer)1.343242432);
+//nowdoc é como '' heredoc é como ""
+echo <<<'EOH'
+sdfsfs\n
+sfsfsfd
+sfsfsf
+EOH;
+
+
+$var = 5;
+
+echo "o valor de { $var}";//non rula
+echo "o valor de {$var}";//rula e non se permiten expresions
+
+$valor ="sdfsfsfdsf5";
+//se é un string que conten un valor numerico so é casteado implicitamente
+var_dump($valor[strlen($valor)-1] + 1);
+*/
+
+
+
